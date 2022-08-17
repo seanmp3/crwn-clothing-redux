7a327a66aeed8bc80b4b66982f60093cbeceea8f
sean-mahan <seanmahansean@gmail.com>
2022-08-17 07:49:49 -0500

    added cart reducer

--- src/contexts/user/index.js
+++ src/contexts/user/index.js
@@ -1,5 +1,7 @@
 import {createContext, useEffect, useReducer} from "react"
 
+import {createAction} from "../../utils/reducer"
+
 import {
   onAuthStateChangedListener, 
   createUserDocumentFromAuth
@@ -37,7 +39,7 @@ export const UserProvider = ({children}) => {
   const value = {currentUser}
 
   const setCurrentUser = (user) => {
-    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
   }
 
   useEffect(() => {
